cf85ed499e04a4cadc93d5db5410850eee782c4c
helium01 <fajarw935@gmail.com>
2021-09-26 23:21:57 +0700

    upload pertama

---
+++ app/src/main/java/com/example/tutorbc/klaskontak.java
@@ -0,0 +1,57 @@
+package com.example.tutorbc;
+
+public class klaskontak {
+    private String nama, telepon, sosmed, alamat, foto;
+
+    public klaskontak() {
+    }
+
+    public klaskontak(String nama, String telepon, String sosmed, String alamat, String foto) {
+        this.nama = nama;
+        this.telepon = telepon;
+        this.sosmed = sosmed;
+        this.alamat = alamat;
+        this.foto = foto;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+
+    public String getSosmed() {
+        return sosmed;
+    }
+
+    public void setSosmed(String sosmed) {
+        this.sosmed = sosmed;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+}
